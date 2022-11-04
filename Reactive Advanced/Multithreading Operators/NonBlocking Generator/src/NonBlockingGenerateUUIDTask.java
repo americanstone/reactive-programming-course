53d644fac4dc8d90b23328b2f45231e83b9d5b1d
Oleh Dokuka <oleh.dokuka@icloud.com>
2022-11-04 03:55:22 -0400

    restructure

---
+++ Reactive Advanced/Multithreading Operators/NonBlocking Generator/src/NonBlockingGenerateUUIDTask.java
@@ -0,0 +1,12 @@
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+public class NonBlockingGenerateUUIDTask {
+
+	static UUIDGenerator uuidGenerator;
+
+	public static Mono<String> generateRandomUUID() {
+		return Mono.defer(() -> Mono.just(uuidGenerator.secureUUID()))
+				.subscribeOn(Schedulers.boundedElastic());
+	}
+}
---
+++ Reactive Advanced/Multithreading Operators/NonBlockingTextFileStream/src/NonBlockingTextFileStreamTask.java
@@ -0,0 +1,15 @@
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class NonBlockingTextFileStreamTask {
+
+	static Files files;
+
+	public static Flux<String> readFile(String filename) {
+		return Flux.using(() -> files.lines(Paths.get(filename)), Flux::fromStream, Stream::close)
+				.subscribeOn(Schedulers.boundedElastic());
+	}
+}
